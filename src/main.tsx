56f2f2478ce1dde322920c9f2732afb2bc1cc75e
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2024-06-10 13:50:57 +0600

    [done] removed tailwindcss material

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 import ReduxProvider from "./redux/ReduxProvider.tsx";
-import { ThemeProvider } from "@material-tailwind/react";
 import OpenSettingProvider from "./context/OpenSettingContext.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <ThemeProvider>
-            <ReduxProvider>
-                <OpenSettingProvider>
-                    <App />
-                </OpenSettingProvider>
-            </ReduxProvider>
-        </ThemeProvider>
+        <ReduxProvider>
+            <OpenSettingProvider>
+                <App />
+            </OpenSettingProvider>
+        </ReduxProvider>
     </React.StrictMode>
 );
